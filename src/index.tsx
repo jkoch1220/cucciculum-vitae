0d1cd5f0746312a3e945d266c2bae2b86c2dbafa
Julian Koch <j.koch@tarent.de>
2021-07-26 19:06:10 +0200

    Create React App and add MainPage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainPage from "./pages/main/MainPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainPage() {
+    return (
+        <div>
+            <h1>
+                MAINPAGE
+            </h1>
+        </div>
+    );
+}
+
+export default MainPage;
